d526d0c48c97f2148bb813df3d11f082a77588d7
Anmol Anand <anmolanand2k15@gmail.com>
2022-08-02 21:31:00 +0530

    Added Some Changes

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const fs = require('fs');
-const { Client , LegacySessionAuth } = require('whatsapp-web.js');
+const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const grp = require('./config.json');
 
-const SESSION_FILE_PATH = './session.json';
-
-let sessionData;
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
-}
-
-const client = new Client({
-    authStrategy: new LegacySessionAuth({
-        session: sessionData
-    })
-});
+const client = new Client();
 
 client.on('qr', (qr) => {
     qrcode.generate(qr, {small: true});
@@ -28,15 +17,6 @@ client.on('ready', () => {
             myGroup.id._serialized,
             "Hello !! I Am Ready !!"
         );
-    });
-});
-
-client.on('authenticated', (session) => {
-    sessionData = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-        if (err) {
-            console.error(err);
-        }
     });
 });
 
